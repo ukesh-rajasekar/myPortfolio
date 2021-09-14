796820a3722a3bb27e6ff4496627b33d1e0b88a5
Ukesh Rajasekar <z5288900@ad.unsw.edu.au>
2021-09-14 13:08:10 +1000

    navbar fix

--- src/pages/About.js
+++ src/pages/About.js
@@ -19,16 +19,15 @@ export default function About() {
                <Typography>
                   I'm from the southern part of India, Tamil Nadu, the land of
                   temples and festivals 🎡🎠. I moved to Sydney in February 7
-                  2020, to pursue my masters import 'module' Information
-                  Technology at UNSW. I'm currently in my last term of my uni
-                  and will be graduating in November 2021.
+                  2020, to pursue my masters in Information Technology at UNSW.
+                  I'm currently in my last term of my uni and will be graduating
+                  in November 2021.
                </Typography>
                <br />
                <Typography>
                   Personally, I'm a big film buff who is constantly quoting
-                  movies. I love listening to music 🎶 and going to beaches 🌊,
-                  and travelling 🚐 a lot. I try to get away to those as much as
-                  possible.
+                  movies. I love music 🎶, beaches 🌊, and travelling 🚐 a lot.
+                  I try to get away to those as much as possible.
                </Typography>
                <br />
             </div>
